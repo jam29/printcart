8ca3734e07ed415ce4abc32d7ecd303e432e15e2
JBC <jb.cavarec@orange.fr>
2017-09-27 14:10:04 +0200

    recup url mac

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//----------------------------------------------------------------------
 // Kerawen © Juillet 2017. (Cavarec JB).
 // Relais node pour impression de tickets (raspberryPi -> epson tickets).
 // ➠ Détection adresse MAC et transformation (uppercase + trim ":")
@@ -5,16 +6,27 @@
 // ➠ enregistrement dans la table liaison macAddress:socketId 
 // ➠  Attente de l'évènement d'impression "printkbox" avec les données
 // ➠  Requête http vers le serveur php local pour impression  
+// ➠  Ajout generation automatique de l'url afficheur 
+// 
 //----------------------------------------------------------------------
 var io = require('socket.io-client');
 var request = require('request');
-var changeCase = require("change-case");
+var changeCase = require('change-case');
+var fs = require('fs-extra');
 
 require('getmac').getMac(function(err,macAddress){
     if (err)  throw err
     var MACADDRESS = changeCase.upperCase(macAddress).replace(/:/g, '');
    
-    var socket = io.connect('https://screen.kerawen.com:3050', { secure: true, rejectUnauthorized: false,reconnect: true});
+    var socket  = io.connect('https://screen.kerawen.com:3050', { secure: true, rejectUnauthorized: false,reconnect: true});
+    var socket2  = io.connect('https://screen.kerawen.com:3030', { secure: true, rejectUnauthorized: false,reconnect: true});
+
+    socket2.on('connect', function() { 
+        socket2.emit("urlraspberry",MACADDRESS,function(data){ 
+             console.log(data);
+             fs.writeFile('/home/pi/urll',data.url_longue,function(err){ })
+        })
+    })
 
     // version http
     // var socket = io.connect('http://screen.kerawen.com:3040', { reconnect: true });
